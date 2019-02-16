02a7597e4c1bbfa8518e37231712e3cb74c6409d
Jean THOMAS <git0@pub.jeanthomas.me>
2019-02-16 15:29:20 +0100

    Fix register bug, add set_leds() & set_buck() functions

--- main.c
+++ main.c
@@ -6,20 +6,33 @@
 
 static void cfg_hw(void) {
 	/* LEDS */
-	PB_ODR |= GREEN_LED | RED_LED;
+	PB_DDR |= GREEN_LED | RED_LED;
 	PB_CR1 |= GREEN_LED | RED_LED;
 
 	/* BUZZER */
-	PD_ODR |= BUZZER;
+	PD_DDR |= BUZZER;
 	PD_CR1 |= BUZZER;
 
 	/* POWER BUTTON */
-	PD_ODR &= ~PWR_BTN;
+	PD_DDR &= ~PWR_BTN;
 	PD_CR1 &= ~PWR_BTN;
 
 	/* BUCK ENABLE */
-	PC_ODR |= BUCK_EN;
+	PC_DDR |= BUCK_EN;
 	PC_CR1 |= BUCK_EN;
+}
+
+static void set_leds(bool red, bool green) {
+	PB_ODR |= (red ? RED_LED : 0) | (green ? GREEN_LED : 0);
+	PB_ODR &= ~((!red ? RED_LED : 0) | (!green ? GREEN_LED : 0));
+}
+
+static void set_buck(bool en) {
+	if (en) {
+		PC_ODR |= BUCK_EN;
+	} else {
+		PC_ODR &= ~BUCK_EN;
+	}
 }
 
 static bool read_btn(void) {
